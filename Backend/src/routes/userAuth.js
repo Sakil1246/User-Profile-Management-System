3f757c3f12f3e0229c95c034bbe6c2b3380a0907
Sakil1246 <sakilahmed345677@gmail.com>
2025-03-29 12:37:16 +0530

    created login route with token

--- Backend/src/routes/userAuth.js
+++ Backend/src/routes/userAuth.js
@@ -3,6 +3,7 @@ const userRouter=express.Router();
 const User=require("../models/user");
 const bcrypt=require("bcrypt");
 const validateSignUp = require("../utils/validation");
+const jwt=require("jsonwebtoken");
 
 
 userRouter.post("/user/signUp",async(req,res)=>{
@@ -24,10 +25,42 @@ userRouter.post("/user/signUp",async(req,res)=>{
     })
 
     const data=await user.save();
+    console.log(data);
+    const userId=data._id;
+    const token=jwt.sign(userId,"user@manaage");
+    res.cookie("token",token);
+
     res.status(201).json({
         message:"User created successfully",
         data:data
     });
+    }
+    catch(err){
+        //console.log(err);
+        res.status(400).json({ error: "ERROR: " + err.message });
+    }
+})
+
+userRouter.post("/user/login",async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user=await User.findOne({email:email});
+        if(!user){
+            return res.status(400).json({error:"Invalid credentials"});
+        }
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({error:"Invalid credentials"});
+        }
+        const userId=user._id.toString();
+        //console.log(userId);
+        const token=jwt.sign(userId,"user@manaage");
+        res.cookie("token",token);
+        res.status(200).json({
+            message:"User logged in successfully",
+            data:user
+        });
+
     }
     catch(err){
         //console.log(err);
